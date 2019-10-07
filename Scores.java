b46588c9697aebdd9da1d1566cb3b2563a34a92f
wcs2807 <55217164+wcs2807@users.noreply.github.com>
2019-10-06 20:20:31 -0400

    Add files via upload

--- Scores.java
+++ Scores.java
@@ -1,6 +1,6 @@
 // ITP-120 001M FA19
 // Assignment 2
-// Version 1.1
+// Version 1.2
 //   ~~~~~~~~~~~~~~~Names~~~~~~~~~~~~~
 //   |*William Sutton* - Trevor Saflin|
 //   |Stuart McAlpine - Hassen Shakeel|
@@ -12,11 +12,12 @@
 // 1.0 Fixed formatting error with shotNum being < 10 and score > 9 that would result in an extra space being added between the score and the "|"
 //	Added "smart formatting" to line below the quarter marker
 // 1.1 Minor formatting issues and fixes to make program more like the assignment example
-// 1.1.1 spiced up victory strings
+// 1.2 Added minor GUI elements and comments
 //
+import javax.swing.*; 
 import java.util.*;
 public class Scores{
-   static final Scanner sc = new Scanner(System.in);
+   static Scanner sc = new Scanner(System.in);
    public static void main(String[] args){
       //Vars
       String name1, name2;
@@ -26,22 +27,21 @@ public class Scores{
       String placeHolder = "";
       
       //end Vars
-      System.out.println("Welcome to the basketball points simulator!\nThe player with the most points wins. Good Luck!");
       do{
-         name1 = getString("Please input the name of the first player(within 12 characters):"); 
+         name1 = getString("Please input the name of the first player (within 12 characters):");
          if(name1.length() > 12){
-            System.out.println("You name has too many characters. Please put less than 13 characters");
+            JOptionPane.showMessageDialog(null, "You name has too many characters. Please enter less than 13 characters", "Sorry", JOptionPane.WARNING_MESSAGE);
          }
       }while(name1.length() > 12);
       do{
-         name2 = getString("Please input the name of the second player(within 12 characters):"); 
+         name2 = getString("Please input the name of the second player (within 12 characters):");
          if(name2.length() > 12){
-            System.out.println("You name has too many characters. Please put less than 13 characters");
+            JOptionPane.showMessageDialog(null, "You name has too many characters. Please enter less than 13 characters", "Sorry", JOptionPane.WARNING_MESSAGE);
          }
       }while(name2.length() > 12);
       for(int s = 0; s < (quarters.length); s++){
          
-         for(int x = 0; x < (seconds.length); x++){          
+         for(int x = 0; x < (seconds.length); x++){   
             if(x == 0){
                System.out.println("*******************************************QUARTER " + (s + 1) + "*******************************************");
                System.out.print("Shot#");
@@ -52,12 +52,12 @@ public class Scores{
                placeHolder = name1 + " Score";
                System.out.print(placeHolder);
                returnSpaces(20 - placeHolder.length());
-               placeHolder = "* " + name2 + " Shot";//name2 begins
+               placeHolder = "  " + name2 + " Shot";
                System.out.print(placeHolder);
                returnSpaces(23 - placeHolder.length());
                System.out.print(name2 + " Score");
                System.out.println();
-            }         
+            }     
             if(x % 2 == 0){
                holder = generateShot();
                score1 += holder;
@@ -72,8 +72,8 @@ public class Scores{
             
          }
       }
-      compare(name1, name2, score1, score2, " won it! They won the Game! Incredible!");
-      
+      compare(name1, name2, score1, score2, " won it!");
+      JOptionPane.showMessageDialog(null, "                             Game over!", "", JOptionPane.PLAIN_MESSAGE);
    }
 	//Method: returnSpaces(int num)
 	//Description: prints spaces
@@ -85,14 +85,14 @@ public class Scores{
       }
    }
 	//Method: displayNums1(String name1, int score1, int holder, int shotNum)
-	//Description: displays the shot number, type, and score
+	//Description: displays scores
 	//Precondition: recieves a String name1, int score1, int holder, int shotNum
 	//Postcondition: N/A
    public static void displayNums1(String name1, int score1, int holder, int shotNum){
-      System.out.print(shotNum);  
+      System.out.print(shotNum);
       if(shotNum < 10){
          System.out.print("         ");
-         System.out.print(displayShot(holder));     
+         System.out.print(displayShot(holder));      
          if(score1 < 10) {
         	 System.out.print(score1 + "                    ");//20
          }
@@ -102,8 +102,8 @@ public class Scores{
       }
       else{
          System.out.print("        ");
-         System.out.print(displayShot(holder));    
-         if(score1 < 100){       
+         System.out.print(displayShot(holder));      
+         if(score1 < 100){        
             if(score1 < 10){
                System.out.print(score1 + "                    ");
             }
@@ -118,7 +118,7 @@ public class Scores{
       }
    }
 	//Method: displayNums2(String name1, int score1, int holder, int shotNum)
-	//Description: displays the shot number, type, and score
+	//Description: displays scores
 	//Precondition: recieves a String name1, int score1, int holder, int shotNum
 	//Postcondition: N/A
    public static void displayNums2(String name1, int score1, int holder, int shotNum){
@@ -129,6 +129,7 @@ public class Scores{
 	//Precondition: recieves a String name1, String name2, int score1, int score2, String prompt
 	//Postcondition: N/A
    public static void compare(String name1, String name2, int score1, int score2, String prompt){
+
       if(score1 > score2){
          System.out.println(name1 + prompt);
       }
@@ -136,19 +137,18 @@ public class Scores{
          System.out.println(name2 + prompt);
       }
       else{
-         System.out.println("Tie game..Go Home..See ya' later!");
+         System.out.println("Tie game.. Go Home.. See ya' later!");
+         JOptionPane.showMessageDialog(null, "Tie game.. Go Home.. See ya' later!", "Game over!", JOptionPane.PLAIN_MESSAGE);
       }
    }
    public static String displayShot(int num){//15 spaces
-
+ 
       if(num == 0){
          return "Airball               ";
       }
-
       else if(num == 1){
          return "Foul Ball             ";
       }
-
       else if(num == 2){
          return "Two Points!           ";
       }
@@ -157,24 +157,22 @@ public class Scores{
       }
    }
 	//Method: generateShot()
-	//Description: generates a random shot number
+	//Description: generates a random number
 	//Precondition: N/A
 	//Postcondition: returns a  public static int
    public static int generateShot(){
       return (int)(Math.random() * 4);
    }
 	//Method: getString(String prompt)
-	//Description: returns a string
+	//Description: gets string input
 	//Precondition: recieves a String prompt
 	//Postcondition: returns a  public static String
    public static String getString(String prompt){
       String input = "";
-   
       while(input.equals("")){
          System.out.println(prompt);
          input = sc.nextLine();
-         input = input.trim();
-    
+         input = input.trim();    
          if(input.equals("")){
             System.out.println("Please enter in a string");
          }
