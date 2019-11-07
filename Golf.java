74ed3237ddd6dc60a38aa5a1ef43f4669bc0eef4
Tsaflin <55217098+Tsaflin@users.noreply.github.com>
2019-11-06 20:08:47 -0500

    Add files via upload

--- Golf.java
+++ Golf.java
@@ -15,64 +15,113 @@
 import javax.swing.*; // GUI
 import java.util.*; // Scanner and utilities
 
-public class Golf { // Program name
+public class Golf // Program name
+{                 
    static Scanner sc = new Scanner(System.in); // Scanner
    public static void main(String[] args) // Main method
    {
-      // Variable declaration
    
-   int numPlayers;
-   int count = 0;
-   
-   ArrayList<String> playerList = new ArrayList<>();
+   int size = 0;   
+   String[][] play;
+   while (size < 1)
+      {
+         size = getInt("pleaase enter int");
+         play = new String[size][size];
+         System.out.println("length is " + play.length); // print array size
 
+      } // end while loop
    
-   String[] playerNames[] // The first array will store the Player Name and you must use a getter method
-                           // to get the Player Name and store it in the Player Name array.
-                         
-      // The second array will store the score for the first day of the tournament, 
-//   int dayOneScore[]       // and you must use a getter method to get the first day score, and store it
-                           // in the first day score array. Use a while loop to validate the value of the 
-                           // score to make sure that it falls in the range of 0 to 95 inclusive. 
-                           
-      // The third array will store the score for the second day of the tournament, 
-//   int dayTwoScore[]       // and you must use a getter method to get the second day score, and store it 
-                           // in the second day score array. Use a while loop to validate the value of the score
-                           // to make sure that it falls in the range of 0 to 95 inclusive
-                            
-      // The fourth array will store the combined score, and you must calculate
-//   int combinedScore[]     // the combined score which is the first day score (stored in the first day score array) 
-                           // and add it to the second day score (stored in the second day score array) and store
-                           // the result in the combined score array. 
- 
-
-   // You begin by asking the user how many players for whom they want to enter scores.
-   System.out.println("Enter the number of players: ");
-   numPlayers = sc.nextInt(); 
-   // Validate the response with a while loop to ensure that the value entered is between 0 and 40. (or 200?)
-   while (numPlayers != -1 && count <= 200)
-   {
-      playerList = new int[numPlayers];
-      //// polite error message if # players entered is above 200?
    
-      // Once you have a valid response, a for loop is entered for processing 4 parallel arrays.
-      // The size of each of the parallel arrays is equal to the number of players entered by the user. ***
-      for {
-         
-         }  // End of for loop
-         
-   }  // End of while loop
-         
-      
-   // If you have at least 1 player, print the contents of the parallel arrays meaning the player name, first day score, and
-   // second day score, and combined score for each player, the leader, and anyone within 10 strokes of the leader. End with
-   // a message See Ya’ At the 19th Hole as shown.   
-   System.out.println("See Ya' At the 19th Hole");
+   // Once you have a valid response, a for loop is entered for
+   // processing 4 parallel arrays.
+  
    
+   
+   for (int i = 0; i < size; i++)
+		   {
+	   
+			   play[i] = sc.nextLine();
+   		
+		   } // end for loop
    
    } // end of main
+   public static int getInt(String prompt) {
+	   int b = 0;
+	   System.out.println(prompt);
+	   b = sc.nextInt();
+	   return b;
+   }
+   public static String getString(String prompt) {
+	   String a = "";
+	   System.out.println(prompt);
+	   a = sc.nextString();
+	   return a;
+   }
+
 } // end of file
 
+
+
+
+
+
+
+
+
+
+
+
+
+   // Get scores
+
+// import java.util.*;
+
+// public class expl{
+//    static Scanner sc = new Scanner(System.in);
+//    public static void main(String[] args){
+//       int size = sc.nextInt();
+//       int[][] play = new int[size][size];
+//    }
+// }
+//
+
+
+
+// --------------------- 
+
+// // // Ch.7 Slide #30 - Finding the Highest Value in an array
+// int [] numbers = new int[50];
+// int highest = numbers[0];
+// for (int i = 1; i < numbers.length; i++)
+// {
+//	if (numbers[i] > highest)
+//		highest = numbers[i];
+// }
+
+
+// // // Ch.7 Slide #30 - Finding lowest value
+// int lowest = numbers[0];
+// for (int i = 1; i < numbers.length; i++)
+// {
+//	if (numbers[i] < lowest)
+//		lowest = numbers[i];
+// }
+
+// **** Ch.7 Slide #50 - Printing scores array
+
+
+
+
+// import java.util.*;
+
+// public class expl{
+//    static Scanner sc = new Scanner(System.in);
+//    public static void main(String[] args){
+//       int size = sc.nextInt();
+//       int[][] play = new int[size][size];
+//    }
+// }
+//
 
 
 
