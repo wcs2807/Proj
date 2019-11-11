328ede8a8431c21eb4fdf7c586cdf23b04e99c47
wcs2807 <55217164+wcs2807@users.noreply.github.com>
2019-11-10 18:57:59 -0800

    Add files via upload

--- Golf.java
+++ Golf.java
@@ -1,148 +1,93 @@
 // ITP-120 001M FA19
 // Assignment 3
-// Version 0.1
-//
+// Version 1.0
 //   ~~~~~~~~~~~~~~~Names~~~~~~~~~~~~~
 //   |*William Sutton* - Trevor Saflin|
 //   |Stuart McAlpine - Hassen Shakeel|
 //   |Daanish Ahmed - Brian Pourkesali|
 //   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// Description: Score tracker for Masters Golf Tournament
+// Description: This program calculates the scores of a golf game
 // Version Changes:
-//
+// 1.0 Did the thing
+import javax.swing.*; 
+import java.util.*;
 
-import javax.swing.*; // GUI
-import java.util.*; // Scanner and utilities
-
-public class Golf // Program name
-{                 
-   static Scanner sc = new Scanner(System.in); // Scanner
-   public static void main(String[] args) // Main method
-   {
-   
-   int size = 0;   
-   String[][] play;
-   while (size < 1)
-      {
-         size = getInt("pleaase enter int");
-         play = new String[size][size];
-         System.out.println("length is " + play.length); // print array size
-
-      } // end while loop
-   
-   
-   // Once you have a valid response, a for loop is entered for
-   // processing 4 parallel arrays.
-  
-   
-   
-   for (int i = 0; i < size; i++)
-		   {
-	   
-			   play[i] = sc.nextLine();
-   		
-		   } // end for loop
-   
-   } // end of main
-   public static int getInt(String prompt) {
-	   int b = 0;
-	   System.out.println(prompt);
-	   b = sc.nextInt();
-	   return b;
-   }
-   public static String getString(String prompt) {
-	   String a = "";
-	   System.out.println(prompt);
-	   a = sc.nextString();
-	   return a;
-   }
-
-} // end of file
-
-
-
-
-
-
-
-
-
-
-
-
-
-   // Get scores
-
-// import java.util.*;
-
-// public class expl{
-//    static Scanner sc = new Scanner(System.in);
-//    public static void main(String[] args){
-//       int size = sc.nextInt();
-//       int[][] play = new int[size][size];
-//    }
-// }
-//
-
-
-
-// --------------------- 
-
-// // // Ch.7 Slide #30 - Finding the Highest Value in an array
-// int [] numbers = new int[50];
-// int highest = numbers[0];
-// for (int i = 1; i < numbers.length; i++)
-// {
-//	if (numbers[i] > highest)
-//		highest = numbers[i];
-// }
-
-
-// // // Ch.7 Slide #30 - Finding lowest value
-// int lowest = numbers[0];
-// for (int i = 1; i < numbers.length; i++)
-// {
-//	if (numbers[i] < lowest)
-//		lowest = numbers[i];
-// }
-
-// **** Ch.7 Slide #50 - Printing scores array
-
-
-
-
-// import java.util.*;
-
-// public class expl{
-//    static Scanner sc = new Scanner(System.in);
-//    public static void main(String[] args){
-//       int size = sc.nextInt();
-//       int[][] play = new int[size][size];
-//    }
-// }
-//
-
-
-
-// --------------------- 
-
-// // // Ch.7 Slide #30 - Finding the Highest Value in an array
-// int [] numbers = new int[50];
-// int highest = numbers[0];
-// for (int i = 1; i < numbers.length; i++)
-// {
-//	if (numbers[i] > highest)
-//		highest = numbers[i];
-// }
-
-
-// // // Ch.7 Slide #30 - Finding lowest value
-// int lowest = numbers[0];
-// for (int i = 1; i < numbers.length; i++)
-// {
-//	if (numbers[i] < lowest)
-//		lowest = numbers[i];
-// }
-
-// **** Ch.7 Slide #50 - Printing scores array
+public class Golf {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		List<String> names = new ArrayList();
+		List<String> names2 = new ArrayList();
+		String holder = "";
+		int[] first, second, combined;
+		int leader = 0, size = 0;
+		while(size > 40 || size < 1) {
+			size = getInt("Please enter in the amount of people:");
+         sc.nextLine();
+			if(size != 0) {
+				first = new int[size];
+				second = new int[size];
+				for(int i = 0; i < size; i++) {
+					System.out.println("--------------Player " + (i + 1) + "------------------");
+					holder = getString("Please enter player name:");
+					names.add(i, holder);
+					first[i] = getInt("Enter in score for the first day");
+					second[i] = getInt("Enter in score for the second day");
+					sc.nextLine();
+				}
+				combined = new int[size];
+				for(int u = 0; u < size; u++) {
+					combined[u] = first[u] + second[u];
+				}
+				leader = 0;
+				for(int w = 0; w < size; w++) {
+					if(combined[w] > combined[leader]) {
+						leader = w;
+					}
+				}
+				for(int p = 0; p < size; p++) {
+					if(combined[p] >= (combined[leader] - 10) && leader != p) {
+						names2.add(names.get(p));
+					}
+				}
+				Display(names, names2, first, second, combined, leader, size);
+			}
+			else {
+				System.out.println("Entered 0 players. terminating process");
+			}
+		}
+		
+	}
+	public static void Display(List<String> names, List<String> names2, int[] first, int[] second, int[] combined, int leader, int size) {
+		System.out.println("PlayerID        Day1        Day2        Combined");
+		for(int i = 0; i < size; i ++) {
+			System.out.println(names.get(i) + "        " + first[i] + "        " + second[i] + "        " + combined[i]);
+		}
+		System.out.println("____");
+		System.out.println("Leader: " + names.get(leader));
+		System.out.println("____");
+		System.out.println("Players within 10 points");
+		for(int q = 0; q < names2.size(); q++) {
+			System.out.println(names2.get(q));
+		}
+		System.out.println("See ya at the 19th hole!");
+	}
+	public static String getString(String prompt) {
+		String q;
+		System.out.println(prompt);
+		q = sc.nextLine();
+		return q;
+	}
+	public static int getInt(String prompt) {
+		int a = 0;
+		while(a > 95 || a < 1) {
+			System.out.println(prompt);
+			a = sc.nextInt();
+			if(a > 95 || a < 1) {
+				System.out.println("enter valid num");
+			}
+		}
+		return a;
+		
+	}
+}
